915683123922324d8f861d3033b605820f28527f
Trilisen <jonmcg01@gmail.com>
2020-12-27 16:38:22 +0100

    Added more basic php ripped from the instructions

--- views/header.php
+++ views/header.php
@@ -7,11 +7,10 @@
 
     <title><?php echo $config['title']; ?></title>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/styles/style.css">
 </head>
 
 <body>
     <?php require __DIR__ . '/navigation.php'; ?>
 
-    <div class="container py-5">
+    <div>
