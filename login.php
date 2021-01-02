15eff1cc78a0bf3dadb124c72bfe7e11c2c5e122
Trilisen <jonmcg01@gmail.com>
2021-01-02 15:18:57 +0100

    Added code from intructions to multiple php files and written the register.php to insert

--- login.php
+++ login.php
@@ -5,19 +5,48 @@
     <h1>Login</h1>
 
     <form action="app/users/login.php" method="post">
-        <div class="form-group">
+        <div>
             <label for="email">Email</label>
-            <input class="form-control" type="email" name="email" id="email" placeholder="francis@darjeeling.com" required>
-            <small class="form-text text-muted">Please provide the your email address.</small>
+            <input type="email" name="email" id="email" placeholder="francis@darjeeling.com" required>
+            <small>Please provide the your email address.</small>
         </div><!-- /form-group -->
 
-        <div class="form-group">
+        <div>
             <label for="password">Password</label>
-            <input class="form-control" type="password" name="password" id="password" required>
-            <small class="form-text text-muted">Please provide the your password (passphrase).</small>
+            <input type="password" name="password" id="password" required>
+            <small>Please provide your password.</small>
         </div><!-- /form-group -->
 
-        <button type="submit" class="btn btn-primary">Login</button>
+        <button type="submit">Login</button>
+    </form>
+</article>
+
+<article>
+    <h1>Register</h1>
+    <form action="app/users/register.php" method="post">
+        <div>
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" placeholder="Francis" required>
+            <small>Please provide your username.</small>
+        </div>
+
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" placeholder="francis@darjeeling.com" required>
+            <small>Please provide the email address.</small>
+        </div>
+
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" required>
+            <small>Please provide your password.</small>
+        </div>
+
+        <div>
+            <label for="pass-confirm">Confirm password</label>
+            <input type="password" name="pass-confirm" id="pass-confirm" required>
+            <small>Please confirm your password.</small>
+        </div>
     </form>
 </article>
 
